6be3da7b8515b95fcf7ab945b88db63b5efe8969
Palandr1234 <palandr545@gmail.com>
2022-09-28 11:42:45 +0300

    Add weight scaling

---
+++ StyleGAN/utils.py
@@ -0,0 +1,30 @@
+class ScaleWeights:
+    def __init__(self, name):
+        self.name = name
+
+    def compute_weight(self, module):
+        weight = getattr(module, self.name + '_orig')
+        fan_in = weight.data.size(1) * weight.data[0][0].numel()
+
+        return weight * sqrt(2 / fan_in)
+        
+    @staticmethod
+    def apply(module, name):
+        fn = ScaleWeights(name)
+
+        weight = getattr(module, name)
+        del module._parameters[name]
+        module.register_parameter(name + '_orig', nn.Parameter(weight.data))
+        module.register_forward_pre_hook(fn)
+
+        return fn
+
+    def __call__(self, module, input):
+        weight = self.compute_weight(module)
+        setattr(module, self.name, weight)
+
+
+def equal_lr(module, name='weight'):
+    ScaleWeights.apply(module, name)
+
+    return module
