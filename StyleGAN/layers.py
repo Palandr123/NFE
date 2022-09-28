2ca94425b1cf77f65168a874875a48e66b73991a
Palandr1234 <palandr545@gmail.com>
2022-09-28 12:06:51 +0300

    Add linear layer

---
+++ StyleGAN/layers.py
@@ -0,0 +1,12 @@
+class ScaledLinear(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        linear = nn.Linear(in_dim, out_dim)
+        linear.weight.data.normal_()
+        linear.bias.data.zero_()
+
+        self.linear = equal_lr(linear)
+
+    def forward(self, x):
+        return self.linear(x)
