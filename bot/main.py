45baedae58c61eb0af8a34bc5f7684fce7959b13
Anna Startseva <66643655+asleepann@users.noreply.github.com>
2022-11-15 21:04:29 +0300

    Add random faces generation

--- bot/main.py
+++ bot/main.py
@@ -13,6 +13,14 @@ def send_welcome(message):
     bot.reply_to(message, 'Hello! This is Neural Face Editor bot!\nSend /face to generate random faces with GAN!')
 
 
+@bot.message_handler(commands=['face'])
+def generate_face(message):
+    img = inference()
+    imgpath = 'image.png'
+    plt.imsave(imgpath, torch.clip(img, 0, 1).permute([1, 2, 0]).detach().cpu().numpy())
+    bot.send_photo(message.chat.id, photo=open(imgpath, 'rb'), caption='Your random face!')
+
+
 @bot.message_handler(func=lambda x: True)
 def echo_all_to_channel(message):
     bot.send_message(message.chat.id, message.text)
